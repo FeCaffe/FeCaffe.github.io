c8326672bb5df81b45b98bf4d59b851cc54733b0
Alex Scott <codetheorist@outlook.com>
2024-06-18 21:38:53 +0100

    Updates

---
+++ _nuxt/BmsR2HZB.js
@@ -0,0 +1,2 @@
+import{c as n,o as r,a}from"./B6h_aQUq.js";const _=(e,t)=>{const c=e.__vccOpts||e;for(const[s,o]of t)c[s]=o;return c},i={},d={class:"d-flex align-items-center justify-content-around w-100",style:{height:"100vh"}},f=a("h1",{class:"text-center"},"FeCaffe",-1),l=[f];function h(e,t){return r(),n("div",d,l)}const x=_(i,[["render",h]]);export{x as default};
+//# sourceMappingURL=BmsR2HZB.js.map
