3ca4b277c76c67e1a5942bcca6ff4b413c7c0c89
Alex Scott <codetheorist@outlook.com>
2024-06-18 21:48:00 +0100

    Updates

--- _nuxt/CzlUBBve.js
+++
@@ -1,2 +0,0 @@
-import{c as n,o as r,a}from"./D2ZJzwxf.js";const _=(e,t)=>{const c=e.__vccOpts||e;for(const[s,o]of t)c[s]=o;return c},i={},d={class:"d-flex align-items-center justify-content-around w-100",style:{height:"100vh"}},f=a("h1",{class:"text-center"},"FeCaffe",-1),l=[f];function h(e,t){return r(),n("div",d,l)}const x=_(i,[["render",h]]);export{x as default};
-//# sourceMappingURL=CzlUBBve.js.map
