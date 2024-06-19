13df21d2ea0bfe4b9804fd5aa83ab09fdc134082
Alex Scott <codetheorist@outlook.com>
2024-06-19 11:38:28 +0100

    Updates

---
+++ _nuxt/XTdztqhn.js
@@ -0,0 +1,2 @@
+import{c as o,b as a,w as r,r as s,l as c,o as n,d as l}from"./VmW3T5D0.js";import{_}from"./DlAUqK2U.js";const d={};function f(e,m){const t=c;return n(),o("div",null,[a(t,{to:"/"},{default:r(()=>[l("Home")]),_:1}),s(e.$slots,"default")])}const p=_(d,[["render",f]]);export{p as default};
+//# sourceMappingURL=XTdztqhn.js.map
