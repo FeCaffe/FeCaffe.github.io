6450a7d479703cbe1debc0a388eb23b376d05389
Alex Scott <codetheorist@outlook.com>
2024-06-19 11:41:40 +0100

    Updates

--- _nuxt/D6io1g-z.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./DlAUqK2U.js";import{c as t,o as s,a as c}from"./VmW3T5D0.js";const o={},n={class:"d-flex align-items-center justify-content-around w-100",style:{height:"100vh"}},a=c("h1",{class:"text-center"},"FeCaffe",-1),r=[a];function _(i,d){return s(),t("div",n,r)}const h=e(o,[["render",_]]);export{h as default};
-//# sourceMappingURL=D6io1g-z.js.map
